8cd943be123de68d5a8f64a100d9615e807ee171
DanyLC91 <daniela.campisi@hotmail.com>
2024-03-03 10:51:37 +0000

    Setting up the app and importing Ract libraries

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'; // Importing the React library
+import ReactDOM from 'react-dom/client'; // Importing ReactDOM for rendering
+import App from './App'; // Importing the main App component
+import reportWebVitals from './reportWebVitals'; // Importing a function for reporting web vitals
 
+// Creating a root React element to render the application.
+//It will serve as the entry point for rendering the application.
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Rendering the main App component inside a StrictMode wrapper
 root.render(
   <React.StrictMode>
-    <App />
+    <App /> {/* Rendering the main App component */}
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Reporting web vitals to monitor performance
 reportWebVitals();

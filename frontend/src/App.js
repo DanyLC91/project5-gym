b271cf0bf2b18a2befc672fa19d3f05f851b2b79
DanyLC91 <daniela.campisi@hotmail.com>
2024-03-03 11:06:12 +0000

    Setting up app routes and main style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import AboutUs from './pages/AboutUs';
+import TheGym from './pages/TheGym';
+import Memberships from './pages/Memberships';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+// Importing necessary routing components from react-router-dom
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/aboutUs" element={<AboutUs/>}/>
+          <Route path="/theGym" element={<TheGym/>}/>
+          <Route path="/memberships" element={<Memberships/>}/>
+        </Routes>
+        <Footer/>
+      </Router>  
     </div>
-  );
+  );//Each <Route> component specifies a path and the corresponding component to render when that path is matched.
 }
-
 export default App;
